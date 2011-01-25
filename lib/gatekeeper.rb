04430123203346673c2d5a04ed5ea5cc6c54d785
fourfour <eric@fourfourmedia.com>
2011-01-25 12:59:06 -0800

    changed README, cleaned up authenticate

--- lib/gatekeeper.rb
+++ lib/gatekeeper.rb
@@ -2,14 +2,8 @@ module Gatekeeper
   
   def gatekeeper_authenticate
     @gatekeeper_config ||= YAML.load File.open "#{Rails.root}/config/gatekeeper.yml"
-    
-    @users = {}
-    @users[@gatekeeper_config['USERS']['name']] = @gatekeeper_config['USERS']['password']
-    
-    self.config.secret = @gatekeeper_config['USERS']['secret']
-    
-    authenticate_or_request_with_http_digest(@gatekeeper_config['USERS']['app_name']) do |name|
-      @users[name]
+    authenticate_or_request_with_http_basic(@gatekeeper_config['USERS']['app_name']) do |username, password|
+      username == @gatekeeper_config['USERS']['name'] && password == @gatekeeper_config['USERS']['password']
     end
   end
   
