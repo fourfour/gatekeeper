55c8fe93c2d25aa90b1e234e635f8384a561fd97
Joshua Kifer <joshua.kifer@gmail.com>
2011-01-27 15:21:01 -0800

    Adding before_filter, so now just include the gem in the Gemfile and yer good

--- lib/gatekeeper.rb
+++ lib/gatekeeper.rb
@@ -1,4 +1,5 @@
 class ActionController::Base
+  before_filter :gatekeeper_authenticate
   def gatekeeper_authenticate
     @gatekeeper_config ||= YAML.load File.open "#{Rails.root}/config/gatekeeper.yml"
     authenticate_or_request_with_http_basic(@gatekeeper_config['USERS']['app_name']) do |username, password|
