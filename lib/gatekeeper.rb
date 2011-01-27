be1e603a2b2b871b3bb75e4a4898a7cca3edf2a1
Joshua Kifer <joshua.kifer@gmail.com>
2011-01-27 11:55:44 -0800

    Removed Gatekeeper module in favor of patching ActionController::Base, so as to remove the need to include the module in your controller.

--- lib/gatekeeper.rb
+++ lib/gatekeeper.rb
@@ -1,10 +1,8 @@
-module Gatekeeper
-  
+class ActionController::Base
   def gatekeeper_authenticate
     @gatekeeper_config ||= YAML.load File.open "#{Rails.root}/config/gatekeeper.yml"
     authenticate_or_request_with_http_basic(@gatekeeper_config['USERS']['app_name']) do |username, password|
       username == @gatekeeper_config['USERS']['name'] && password == @gatekeeper_config['USERS']['password']
     end
   end
-  
 end
